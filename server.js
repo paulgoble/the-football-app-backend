125bff8270134d376884263e3e7774246bc05730
Paul Goble <100052818+paulgoble@users.noreply.github.com>
2022-11-06 10:44:23 -0500

    added config for static files and updated PORT

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const standingsRouter = require('./routers/standingsRouter')
 const teamsRouter = require('./routers/teamsRouter')
 
 app.use(cors())
-app.use(express.json())
 app.use(morgan('dev'))
-
+app.use(express.json())
+app.use(express.static('build'))
 app.use('/fixtures', fixturesRouter)
 app.use('/results', resultsRouter)
 app.use('/standings', standingsRouter)
 app.use('/teams', teamsRouter)
 
-const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
